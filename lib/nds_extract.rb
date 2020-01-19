0b668777184c2776039922b679e7205f1c63f4d1
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 18:42:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 
 def gross_for_director(director_data)
   movie_num = 0
-  length = = director_data[:movies]
+  length = = director_data[:movies].length
 end
 
 def directors_totals(nds)
