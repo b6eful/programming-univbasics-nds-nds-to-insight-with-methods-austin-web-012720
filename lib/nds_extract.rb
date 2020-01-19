1fbd268112cd6ae0668c8d3bb4091fbdcc977557
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 18:35:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,8 @@ def gross_for_director(director_data)
   while num_movies < director_data [:movies].length do
     total += director_data[:movies][num_movies][:worldwide_grosses]
     num_movies += 1
-    
+  end
+  total
 end
 
 # Write a method that, given an NDS creates a new Hash
