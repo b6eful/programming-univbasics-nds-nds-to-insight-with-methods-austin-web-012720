2e76f3f440eb3160a2ab95c08cd2613b10619997
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 18:32:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ require 'directors_database'
 def gross_for_director(director_data)
   movies = 0
   total = 0
-  while num_movies
+  while num_movies < 
 end
 
 # Write a method that, given an NDS creates a new Hash
